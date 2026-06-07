f6cd69ffe0d869404f2a8c39d4995625dda72539
vihegde <vinay.hegde123@gmail.com>
2026-06-07 23:35:08 +0530

    Partial changes

---
+++ python-app/template/src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'message':'Hello World',
+        'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+        'hostname':socket.gethostname(),
+        'message':'You are doing g8-!!!! Man)'
+    })
+
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify ({
+        'status':'Up'
+    }), 200
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+
+
+
+
+
+#'
